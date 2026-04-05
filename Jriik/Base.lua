dc65f3a11573923fd028ac41bf88f7f7f19db229
Jriik89 <110338305+sadboy-dev@users.noreply.github.com>
2026-04-05 23:09:48 +0800

    Update Base.lua

--- Jriik/Base.lua
+++ Jriik/Base.lua
@@ -17,15 +17,16 @@ ScreenGui.Parent = game.CoreGui
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 -- SETTING KOTAK UTAMA
+-- POSISI DIATUR TENGAH LAYAR
 MainFrame.Name = "MainFrame"
 MainFrame.Parent = ScreenGui
 MainFrame.BackgroundColor3 = Color3.new(0.05, 0.05, 0.05)
 MainFrame.BackgroundTransparency = 0.2
 MainFrame.BorderColor3 = Color3.new(1, 1, 1)
 MainFrame.BorderSizePixel = 1
-MainFrame.Position = UDim2.new(0.05, 0, 0.1, 0)
+MainFrame.Position = UDim2.new(0.5, -175, 0.5, -140) -- <-- TENGAH
 MainFrame.Size = UDim2.new(0, 350, 0, 280)
-MainFrame.Visible = true -- <-- UBAH JADI TRUE BIAR LANGSUNG MUNCUL
+MainFrame.Visible = true
 
 -- SETTING AREA KONTEN
 ContentArea.Name = "ContentArea"
@@ -56,3 +57,22 @@ local VisualsFrame = CreateFrame("Visuals")
 local LuaFrame = CreateFrame("Lua")
 local ResourcesFrame = CreateFrame("Resources")
 local ConfigFrame = CreateFrame("Config")
+
+-- Header
+
+-- MEMBUAT HEADER
+local Header = Instance.new("TextLabel")
+
+-- SETTING HEADER
+Header.Name = "Header"
+Header.Parent = MainFrame -- Menempel ke MainFrame
+Header.BackgroundColor3 = Color3.new(0.1, 0.1, 0.1)
+Header.BorderColor3 = Color3.new(1, 1, 1)
+Header.BorderSizePixel = 1
+Header.Size = UDim2.new(1, 0, 0, 25) -- Lebar penuh, tinggi 25
+Header.Position = UDim2.new(0, 0, 0, 0) -- Pojok atas
+Header.Font = Enum.Font.GothamBold
+Header.Text = "  Jriik | UI" -- Ada space di depan
+Header.TextColor3 = Color3.new(1, 1, 1)
+Header.TextSize = 12
+Header.TextXAlignment = Enum.TextXAlignment.Left
